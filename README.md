f577d3c7a73fd28dfa7f586568b7a9cba50a9b01
mustapha <mustapha@zidoworld.com>
2020-02-20 10:26:05 +0100

    feat(version): add a defautversion option to apply a version for all languages in once

--- README.md
+++ README.md
@@ -49,6 +49,9 @@ i18next
   // expiration
   expirationTime: 7*24*60*60*1000,
 
+  // Version applied to all languages, can be overriden using the option `versions`
+  defaultVersion: '',
+
   // language versions
   versions: {},
 
@@ -60,6 +63,8 @@ i18next
 - Contrary to cookies behavior, the cache will respect updates to `expirationTime`. If you set 7 days and later update to 10 days, the cache will persist for 10 days
 
 - Passing in a `versions` object (ex.: `versions: { en: 'v1.2', fr: 'v1.1' }`) will give you control over the cache based on translations version. This setting works along `expirationTime`, so a cached translation will still expire even though the version did not change. You can still set `expirationTime` far into the future to avoid this
+
+- Passing in a `defaultVersion` string (ex.: `version: 'v1.2'`) will act as if you applied a version to all languages using `versions` option. 
 
 --------------
 
--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ function getDefaults() {
   return {
     prefix: 'i18next_res_',
     expirationTime: 7 * 24 * 60 * 60 * 1000,
+    defaultVersion: '',
     versions: {},
     store: window.localStorage
   };
@@ -59,12 +60,13 @@ class Cache {
 
     if (local) {
       local = JSON.parse(local);
+      const version = this.getVersion(language);
       if (
         // expiration field is mandatory, and should not be expired
         local.i18nStamp && local.i18nStamp + this.options.expirationTime > nowMS
 
         // there should be no language version set, or if it is, it should match the one in translation
-        && this.options.versions[language] === local.i18nVersion
+        && version === local.i18nVersion
       ) {
         delete local.i18nVersion;
         delete local.i18nStamp;
@@ -80,13 +82,18 @@ class Cache {
       data.i18nStamp = new Date().getTime();
 
       // language version (if set)
-      if (this.options.versions[language]) {
-        data.i18nVersion = this.options.versions[language];
+      const version = this.getVersion(language);
+      if (version) {
+        data.i18nVersion = version;
       }
 
       // save
       this.storage.setItem(`${this.options.prefix}${language}-${namespace}`, JSON.stringify(data));
     }
+  }
+
+  getVersion(language) {
+    return this.options.versions[language] || this.options.defaultVersion;
   }
 }
 
